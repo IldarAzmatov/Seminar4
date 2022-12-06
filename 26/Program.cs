80e356985b24909d2b2e4f6c0a5f857a2536bdb7
Ildar <ildar.azmatov@ya.ru>
2022-12-06 18:33:26 +0500

    программу, которая принимает на вход число и выдаёт количество цифр в числе.

---
+++ 26/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+
+// int sumury()
+// {
+// Console.WriteLine("Введите число ");
+// string number = Console.ReadLine();
+// int count = 0;
+// char[] array = new char[number.Length];
+// for (int i = 0; i < array.Length; i++)
+// {
+//     array[i] = number[i];
+//     count++;
+// }
+// return count;
+// }
+// int sum = sumury();
+// Console.WriteLine(sum);
+
+
+// альтернативный способ решения через подсчет 
+
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count =0;
+while(num>0)
+{
+    num = num/10;
+    count++;
+}
+Console.WriteLine(count);
