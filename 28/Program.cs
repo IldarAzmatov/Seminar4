8edae39a117049510306a202174bad541dd758c6
Ildar <ildar.azmatov@ya.ru>
2022-12-06 17:55:52 +0500

    принимает на вход число N и выдаёт произведение чисел от 1 до N

---
+++ 28/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+int sumury()
+{
+    Console.WriteLine("Введите число ");
+    int A = Convert.ToInt32(Console.ReadLine());
+    int sum = 1;
+    for (int B = 1; B <= A; B++)
+    {
+        sum = sum * B;
+    }
+    return sum;
+}
+
+int sum = sumury();
+Console.WriteLine(sum);
