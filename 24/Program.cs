2db89ed8b6445a2d1ba630c28fd9b6d3e1a6706d
Ildar <ildar.azmatov@ya.ru>
2022-12-06 16:57:04 +0500

    Метод для вычислния суммы от 1 до А

---
+++ 24/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+int sumury()
+{
+    Console.WriteLine("Введите число ");
+    int A = Convert.ToInt32(Console.ReadLine());
+    int sum = 0;
+    for (int B = 1; B <= A; B++)
+    {
+        sum = sum + B;
+    }
+    return sum;
+}
+
+int sum = sumury();
+Console.WriteLine(sum);
+
