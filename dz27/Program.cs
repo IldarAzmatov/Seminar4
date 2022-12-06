8d2413ebe7b4c9841d5e51ba6af8eee470a7cf46
Ildar <ildar.azmatov@ya.ru>
2022-12-06 19:18:43 +0500

    программу, которая принимает на вход число и выдаёт сумму цифр в числе.

---
+++ dz27/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+int sum = 0;
+while (num > 0)
+{
+    sum = num % 10;
+    num = num / 10;
+    count = count + sum;
+}
+Console.WriteLine(count);
